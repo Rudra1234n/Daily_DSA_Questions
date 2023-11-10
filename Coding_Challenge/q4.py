138079885b464ce4c0a94d040397b096fdd014be
Rudra1234n <rudra.pratap2020@vitbhopal.ac.in>
2023-11-10 10:44:43 +0530

    q4

---
+++ Coding_Challenge/q4.py
@@ -0,0 +1,14 @@
+class Solution:   
+    def peakElement(self,arr, n):
+        # Code here
+        l = 0
+        r = n - 1
+        while l <= r:
+            mid = (l + r)//2
+            if (((arr[mid] >= arr[mid-1]) or (mid == 0)) and ((mid == n-1) or (arr[mid] >= arr[mid+1]))):
+                return mid
+            elif arr[mid] < arr[mid + 1]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return 0
