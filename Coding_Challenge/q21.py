2a81ac696ee428080196877e96b9886782688db1
Rudra1234n <rudra.pratap2020@vitbhopal.ac.in>
2023-11-27 10:14:16 +0530

    q21

---
+++ Coding_Challenge/q21.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    #Function to find maximum of each subarray of size k.
+    def max_of_subarrays(self,arr,n,k):
+        a = []
+        b = []
+        c = k-1
+        for i in range(n-c):
+            for j in range(i,k+i):
+                a.append(arr[j])
+            Max = max(a)
+            b.append(Max)
+            a.clear()
+        return b       
+                
