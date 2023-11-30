281a9046fbef85ffcf5591f42a0a2cff61c656fd
Rudra1234n <rudra.pratap2020@vitbhopal.ac.in>
2023-11-30 10:21:04 +0530

    q5

---
+++ C++_Questions/q5.cpp
@@ -0,0 +1,43 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+
+    // Address of opreator - &
+
+    cout<<"The address of num is: "<<&num;
+
+    return 0;
+}
+
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+
+    int *p = &num;
+
+    cout<<"Value is: "<<*p<<endl;
+    cout<<"Address is: "<<p<<endl;
+
+
+    cout<<endl;
+
+    double num1 = 4.5;
+    double *p1 = &num1;
+
+    cout<<"Value is: "<<*p1<<endl;
+    cout<<"Address is: "<<num1<<endl;
+
+    
+
+    return 0;
+}
+
+*/
