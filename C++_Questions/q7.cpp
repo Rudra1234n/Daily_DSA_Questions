6cb6840e3f69168fb06c2f64ea851c54cafeb560
Rudra1234n <rudra.pratap2020@vitbhopal.ac.in>
2023-12-01 10:25:22 +0530

    q7

---
+++ C++_Questions/q7.cpp
@@ -0,0 +1,60 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 5;
+    int x = i;
+    x++;
+
+    cout<<"The value of i: "<<i<<endl;
+    cout<<"The value of x: "<<x<<endl;
+
+
+    return 0;
+}
+
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+
+    int*i = &num;
+    int a = *i;
+
+    a++;
+
+    cout<<"The value of a: "<<a<<endl;
+    cout<<"The value of pointer: "<<*i<<endl;
+    cout<<"The value of num: "<<num<<endl;
+
+
+    return 0;
+}
+
+*/
+
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+    
+    
+    int *i = &num;
+    (*i)++;
+
+
+    cout<<"The value of pointer: "<<*i<<endl;
+    cout<<"The value of num: "<<num<<endl;
+
+    return 0;
+}
+
+*/
