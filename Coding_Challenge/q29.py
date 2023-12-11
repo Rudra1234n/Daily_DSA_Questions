733ba140443fcba10ba4af452c00a01b47885949
Rudra1234n <rudra.pratap2020@vitbhopal.ac.in>
2023-12-11 10:34:01 +0530

    q26

---
+++ Coding_Challenge/q29.py
@@ -0,0 +1,25 @@
+'''
+	Your task is to remove duplicates from given 
+	sorted linked list.
+	
+	Function Arguments: head (head of the given linked list) 
+	Return Type: none, just remove the duplicates from the list.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+#Function to remove duplicates from sorted linked list.
+def removeDuplicates(head):
+    current = head
+    while current:
+        next = current.next
+        if next and current.data == next.data: 
+            current.next = next.next
+            current = current
+        else:
+            current = next
