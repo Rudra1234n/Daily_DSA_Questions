9546f2778c15b0303e4f91b5628bcae2c8b87a32
Rudra1234n <88386897+Rudra1234n@users.noreply.github.com>
2023-12-17 12:47:15 +0530

    Create array6.py

---
+++ Python/array6.py
@@ -0,0 +1,10 @@
+class Solution:
+    #Back-end complete function Template for Python 3
+    
+    #Function to find the leaders in the array.
+    def leaders(self, A, N):
+        res = [A[-1]] #Save current leader 
+        for i in range(N-2,-1,-1):
+            if A[i] >= res[-1]:
+                res.append(A[i])
+        return res[::-1]
