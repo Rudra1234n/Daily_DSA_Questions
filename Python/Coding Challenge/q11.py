528d6651ceac2ed68e73be01c32510d23f5948da
Rudra1234n <88386897+Rudra1234n@users.noreply.github.com>
2024-01-04 01:44:15 +0530

    Create q11.py

---
+++ Python/Coding Challenge/q11.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    
+    def diameter(self, root):
+        def findht(root):
+            nonlocal maxi
+            if root is None:
+                return 0
+            lh = findht(root.left)
+            rh = findht(root.right)
+            maxi = max(maxi, lh + rh+1)
+            return 1 + max(lh, rh)
+        
+        maxi = float("-inf")
+        findht(root)
+        return maxi
