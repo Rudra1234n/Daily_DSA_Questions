0dfd2eba56a26b8df05679969ff4875960d97341
Rudra1234n <88386897+Rudra1234n@users.noreply.github.com>
2024-01-17 23:36:34 +0530

    Create q39.py

---
+++ Coding_Challenge/q39.py
@@ -0,0 +1,13 @@
+class Solution:
+    def inSequence(self, A, B, C):
+        # code here
+        
+            if C == 0:
+                if A == B:
+                    return 1
+                else:
+                    return 0
+            elif (B-A)%C == 0 and (B-A)//C>=0:
+                return 1
+            else:
+                return 0
