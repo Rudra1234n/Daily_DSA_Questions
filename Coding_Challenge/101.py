4676aa7329b949a5317ec12836db2866206d1099
Rudra1234n <88386897+Rudra1234n@users.noreply.github.com>
2024-02-21 22:32:59 +0530

    Create 101.py

---
+++ Coding_Challenge/101.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insert(self, alist, index, n):
+        #code here
+        while index >= 0:
+            if alist[index+1] < alist[index]:
+                alist[index+1], alist[index] = alist[index], alist[index+1]
+            index -= 1
+            
+        
+    #Function to sort the list using insertion sort algorithm.    
+    def insertionSort(self, alist, n):
+        if n == 1:
+            return
+        #code here
+        for index in range(n-1):
+            self.insert(alist, index, n)
