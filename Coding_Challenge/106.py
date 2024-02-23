74e2409c1e183ff9f9898f1cb7459357f3d8c5a2
Rudra1234n <88386897+Rudra1234n@users.noreply.github.com>
2024-02-23 20:21:08 +0530

    Create 106.py

---
+++ Coding_Challenge/106.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+'''
+Function Arguments :
+		@param  : q ( given queue to be used), k(Integer )
+		@return : list, just reverse the first k elements of queue and return new queue
+'''
+
+#Function to reverse first k elements of a queue.
+class Solution:
+    def modifyQueue(self, q, k):
+        # code here
+        q2 = deque()
+        for i in range(k):
+            q2.append(q.popleft())
+        q.extendleft(q2)
+        return q
