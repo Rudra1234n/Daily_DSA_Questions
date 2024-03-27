b3cb53aeb15cd14ed79627d188e0cc57cde156f5
Rudra1234n <88386897+Rudra1234n@users.noreply.github.com>
2024-03-27 19:53:04 +0530

    Create q155.py

---
+++ Coding_Challenge/q155.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfPaths (self, M, N):
+    n=M+N-2
+    r=min(M-1,N-1)
+    num,deno=1,1
+    for i in range(1,r+1):
+        num*=(n+1-i)
+        deno*=i
+    return (num//deno)%self.mod
+
